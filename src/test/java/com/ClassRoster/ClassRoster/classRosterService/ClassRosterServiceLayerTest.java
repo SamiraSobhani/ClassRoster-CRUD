55083f623865fe1945649c6d340b8d03b17140fe
Samira Sobhani <ss121280@yahoo.com>
2020-06-22 12:10:37 -0500

    final touch for test junit

--- src/test/java/com/ClassRoster/ClassRoster/classRosterService/ClassRosterServiceLayerTest.java
+++ src/test/java/com/ClassRoster/ClassRoster/classRosterService/ClassRosterServiceLayerTest.java
@@ -1,6 +1,10 @@
 package com.ClassRoster.ClassRoster.classRosterService;
 
+import com.ClassRoster.ClassRoster.ClassRosterDAO.ClassRosterAuditDAOStubImpl;
+import com.ClassRoster.ClassRoster.ClassRosterDAO.ClassRosterAuditDao;
 import com.ClassRoster.ClassRoster.ClassRosterDAO.ClassRosterDAO;
+import com.ClassRoster.ClassRoster.ClassRosterDAO.ClassRosterDAOStubImpl;
+import com.ClassRoster.ClassRoster.ClassRosterDTO.Student;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,9 +12,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClassRosterServiceLayerTest {
+    private ClassRosterServiceLayer service;
 
+    public ClassRosterServiceLayerTest() {
+        ClassRosterDAO dao = new ClassRosterDAOStubImpl();
+        ClassRosterAuditDao auditDao = new ClassRosterAuditDAOStubImpl();
+
+        service = new ClassRosterServiceLayerImpl(dao, auditDao);
+    }
     @BeforeEach
-    void setUp() {
+     void setUp() {
     }
 
     @AfterEach
@@ -18,15 +29,52 @@ class ClassRosterServiceLayerTest {
     }
 
     @Test
-    void createStudent() {
+    void createStudent()throws Exception {
+        Student student =new Student("0002");
+         student.setFirstName("Sally");
+        student.setLastName("Smith");
+        student.setCohort("Java-Jan-2015");
+        service.createStudent(student);
     }
 
     @Test
-    void testGetAllStudents() {
+    void creatStudentDuplicateId()throws Exception{
+        Student student =new Student("0001");
+        student.setFirstName("Sally");
+        student.setLastName("Smith");
+        student.setCohort("Java-Jan-2015");
+        try{
+            service.createStudent(student);
+            fail("Expected ClassRosterDuplicateIdException was not thrown.");
+        }catch(ClassRosterDuplicateIdException e){
+            return;
+        }
     }
 
     @Test
-    void testGetStudent() {
+    void creatStudentInvalidData() throws Exception{
+        Student student =new Student("0002");
+        student.setFirstName("");
+        student.setLastName("Smith");
+        student.setCohort("Java-Jan-2015");
+
+        try {
+           service.createStudent(student);
+           fail("Expected ClassRosterDataValidation was not thrown. ");
+        }catch (ClassRosterDataValidationException e){
+            return;
+        }
+    }
+
+    @Test
+    void testGetAllStudents()throws Exception {
+        assertEquals(1,service.getAllStudents().size());
+    }
+
+    @Test
+    void testGetStudent() throws Exception{
+       assertNotNull(service.getStudent("0001"));
+       assertNull(service.getStudent("9999"));
     }
 
     @Test
